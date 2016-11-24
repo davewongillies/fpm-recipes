a02878994349a68046dc52e05aa3ca7c6828fab9
David Gillies <david.gillies@bigcommerce.com>
2016-11-24 21:50:16 +1100

    Add extra error handling to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-fpm-cook -q package --tmp-root /tmp --pkg-dir pkg/${DISTRIBUTION}/${RELEASE} recipe.rb
-chown -R ${pkg_dir_uid:-0} pkg
+if fpm-cook -q package --tmp-root /tmp --pkg-dir pkg/${DISTRIBUTION}/${RELEASE} recipe.rb ;then
+    chown --quiet --recursive ${pkg_dir_uid:-0} {cache,pkg}
+    echo "===> Package built successfully"
+else
+    echo "===> ERROR: Something went wrong with your recipe, bailing out."
+    chown --quiet --recursive ${pkg_dir_uid:-0} cache
+    exit 1
+fi
