24feb3f8241561c97d20e595242d44912a742dc1
Dave Wongillies <dave.gillies@gmail.com>
2017-04-27 10:17:04 -0700

    Bump polybar to 3.0.5 and update build_depends

--- polybar/recipe.rb
+++ polybar/recipe.rb
@@ -1,18 +1,41 @@
 class Polybar < FPM::Cookery::Recipe
   name 'polybar'
-  version '2.3.13'
-  revision '1~3a6ca9'
+  version '3.0.5'
+  revision '1'
 
   homepage "https://github.com/jaagr/#{name}"
   source "https://github.com/jaagr/#{name}.git",
     :with      => 'git',
-    :tag       => '3a6ca90fcb9a101fa137d734df49fdb3b73d9d82',
+    :tag       => version,
     :submodule => true
 
   description 'A fast and easy-to-use status bar'
-  build_depends 'cmake', 'cmake-data', 'libboost-dev', 'libfreetype6-dev', 'libxcb1-dev', 'libx11-xcb-dev', 'libxcb-util0-dev', 'libxcb-image0-dev', 'libxcb-randr0-dev', 'libxcb-ewmh-dev', 'libxcb-xkb-dev', 'libxcb-icccm4-dev', 'xcb-proto', 'python-xcbgen', 'i3-wm', 'libiw-dev', 'libasound2-dev', 'libmpdclient-dev', 'pkg-config', 'libfontconfig1-dev', 'libghc-x11-xft-dev'
-
-  depends 'libmpdclient2'
+  build_depends 'cmake',
+    'cmake-data',
+    'libboost-dev',
+    'i3-wm',
+    'libasound2-dev',
+    'libcairo2-dev',
+    'libcurl4-openssl-dev',
+    'libfontconfig1-dev',
+    'libfreetype6-dev',
+    'libghc-x11-xft-dev',
+    'libiw-dev',
+    'libiw-dev',
+    'libjsoncpp-dev',
+    'libmpdclient-dev',
+    'libx11-xcb-dev',
+    'libxcb-ewmh-dev',
+    'libxcb-icccm4-dev',
+    'libxcb-image0-dev',
+    'libxcb-randr0-dev',
+    'libxcb-util0-dev',
+    'libxcb-xrm-dev',
+    'libxcb-xkb-dev',
+    'libxcb1-dev',
+    'pkg-config',
+    'python-xcbgen',
+    'xcb-proto'
 
   def build
     # Crappy workaround for installing build_depends because fpm-cookery uses
