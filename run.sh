ae76d2390b6fe1246a5fc338cc7eedb6cb2c26ec
Dave Wongillies <dave.gillies@gmail.com>
2017-05-11 21:51:04 -0700

    Run apt-get update in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get update -qq -y
 if fpm-cook -q package --tmp-root /tmp --pkg-dir pkg/${DISTRIBUTION}/${RELEASE} recipe.rb ;then
     chown --quiet --recursive ${pkg_dir_uid:-0} {cache,pkg}
     echo "===> Package built successfully"
