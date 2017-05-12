825e8b385f4d094a3cc493359fbfbaba07da840f
Dave Wongillies <dave.gillies@gmail.com>
2017-05-11 21:51:21 -0700

    vimb: bump to webkit2 branch

--- vimb/recipe.rb
+++ vimb/recipe.rb
@@ -1,17 +1,16 @@
 class Vimb < FPM::Cookery::Recipe
   name 'vimb'
-  version '2.11'
-  revision '2-5867b7'
+  version '2.12'
+  revision '1-webkit2'
 
   homepage "https://github.com/fanglingsu/#{name}"
   source "https://github.com/fanglingsu/#{name}.git",
     :with      => 'git',
-    :tag       => '5867b7b413b7cbdf1367781e5244a50465ec2af9',
+    :branch    => 'webkit2',
     :submodule => true
 
   description 'A fast and easy-to-use status bar'
-  build_depends 'libsoup2.4-dev', 'libwebkit-dev', 'libgtk-3-dev', 'libwebkitgtk-3.0-dev'
-  depends 'libwebkitgtk-3.0-0', 'libsoup2.4-1'
+  build_depends 'libsoup2.4-dev', 'libwebkit2gtk-4.0-dev'
 
   def build
     make 'GTK=3'
