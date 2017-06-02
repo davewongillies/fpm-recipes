470006e469a1f554780c910e602a30d20b40dcc7
Dave Wongillies <dave.gillies@gmail.com>
2017-06-01 18:13:23 -0700

    Only update the apt cache when there's build_depends or depends

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-apt-get update -qq -y
+if grep -qE "\s*(depends|build_depends)\s('|\")" recipe.rb; then
+    echo "===> Found depends in recipe.rb, updating apt cache..."
+	apt-get update -qq -y
+fi
+
 if fpm-cook -q package --tmp-root /tmp --pkg-dir pkg/${DISTRIBUTION}/${RELEASE} recipe.rb ;then
     chown --quiet --recursive ${pkg_dir_uid:-0} {cache,pkg}
     echo "===> Package built successfully"
