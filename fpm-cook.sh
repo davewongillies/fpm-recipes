75cfc510c9dc33b4fed493c798f80083fc0e7d6f
David Gillies <dave.gillies@gmail.com>
2017-08-18 13:36:02 -0700

    Mount $SSH_AUTH_SOCK into the container

--- fpm-cook.sh
+++ fpm-cook.sh
@@ -6,7 +6,12 @@ if [[ ! -z "${RECIPE_PATH}" ]]; then
     DISTRO="${2:-ubuntu-xenial}"
     RECIPE_DIR=$(cut -f1 -d/ <<< ${RECIPE_PATH}/)
     RECIPE_RB=$(cut -f2 -d/ <<< ${RECIPE_PATH}/)
-    docker run --rm -v $PWD/${RECIPE_DIR}:/data -e "pkg_dir_uid=${UID}" davewongillies/fpm-recipes:${DISTRO} run.sh ${RECIPE_RB:-recipe.rb}
+    docker run --rm -it \
+        -v $PWD/${RECIPE_DIR}:/data \
+        -v $(dirname ${SSH_AUTH_SOCK}):$(dirname ${SSH_AUTH_SOCK}) \
+        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+        -e "pkg_dir_uid=${UID}" \
+        davewongillies/fpm-recipes:${DISTRO} run.sh ${RECIPE_RB:-recipe.rb}
 else
     echo "You forgot the package dir you want to build"
     exit 1
