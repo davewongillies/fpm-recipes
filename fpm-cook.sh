a1b0332cdf67694e9408c484b12815caee8a5abd
David Gillies <dave.gillies@gmail.com>
2017-08-18 14:25:27 -0700

    Add a docker pull before docker run

--- fpm-cook.sh
+++ fpm-cook.sh
@@ -4,8 +4,13 @@ RECIPE_PATH=${1}
 
 if [[ ! -z "${RECIPE_PATH}" ]]; then
     DISTRO="${2:-ubuntu-xenial}"
+    DOCKER_IMAGE="davewongillies/fpm-recipes"
+
+
     RECIPE_DIR=$(cut -f1 -d/ <<< ${RECIPE_PATH}/)
     RECIPE_RB=$(cut -f2 -d/ <<< ${RECIPE_PATH}/)
+
+    docker pull ${DOCKER_IMAGE}:${DISTRO}
     docker run --rm -it \
         -v $PWD/${RECIPE_DIR}:/data \
         -v $(dirname ${SSH_AUTH_SOCK}):$(dirname ${SSH_AUTH_SOCK}) \
