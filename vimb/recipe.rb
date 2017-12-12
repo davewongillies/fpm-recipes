1a2fea2a3353adc4e4df4675b6fd5387ab442a1f
David Gillies <dave.gillies@gmail.com>
2017-12-11 16:09:12 -0800

    vimb 2.12

--- vimb/recipe.rb
+++ vimb/recipe.rb
@@ -1,16 +1,17 @@
 class Vimb < FPM::Cookery::Recipe
   name 'vimb'
   version '2.12'
-  revision '1-webkit2'
+  revision '3'
 
   homepage "https://github.com/fanglingsu/#{name}"
   source "https://github.com/fanglingsu/#{name}.git",
     :with      => 'git',
-    :branch    => 'webkit2',
+    :tag       => version,
     :submodule => true
 
   description 'A fast and easy-to-use status bar'
-  build_depends 'libsoup2.4-dev', 'libwebkit2gtk-4.0-dev'
+  build_depends 'libsoup2.4-dev', 'libwebkit-dev', 'libgtk-3-dev', 'libwebkitgtk-3.0-dev'
+  depends 'libwebkitgtk-3.0-0', 'libsoup2.4-1'
 
   def build
     make 'GTK=3'
