040cf9feecd2df348a4fa48fc8b4c8fbfdd1fb54
David Gillies <dave.gillies@gmail.com>
2018-01-04 14:40:48 -0800

    ctop: new recipe

---
+++ ctop/recipe.rb
@@ -0,0 +1,18 @@
+class Ctop < FPM::Cookery::Recipe
+  name     'ctop'
+  version  '0.6.1'
+  revision '1'
+
+  description 'Top-like interface for container metrics https://ctop.sh'
+
+  homepage 'https://ctop.sh'
+  source   "https://github.com/bcicen/ctop/releases/download/v#{version}/#{name}-#{version}-linux-amd64"
+  license    'MIT'
+  maintainer 'Dave Wongillies <dave.gillies@gmail.com>'
+
+  def build; end
+  def install
+    bin.install "#{name}-#{version}-linux-amd64", name
+    chmod 0755, bin(name)
+  end
+end
