bb5694af426c7b8f8f741476e60610b47288a18c
Dave Wongillies <dave.gillies@gmail.com>
2018-01-18 10:11:22 -0800

    fpm-cook.sh: only use SSH_AUTH_SOCK when it exists

--- fpm-cook.sh
+++ fpm-cook.sh
@@ -10,12 +10,15 @@ if [[ ! -z "${RECIPE_PATH}" ]]; then
     RECIPE_DIR=$(cut -f1 -d/ <<< ${RECIPE_PATH}/)
     RECIPE_RB=$(cut -f2 -d/ <<< ${RECIPE_PATH}/)
 
+    if [[ ! -z "${SSH_AUTH_SOCK}" ]]; then
+        SSH_SOCK_ARGS="-v $(dirname ${SSH_AUTH_SOCK}):$(dirname ${SSH_AUTH_SOCK}) -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK}"
+    fi
+
     docker pull ${DOCKER_IMAGE}:${DISTRO}
     docker run --rm -it \
         -v $PWD/${RECIPE_DIR}:/data \
-        -v $(dirname ${SSH_AUTH_SOCK}):$(dirname ${SSH_AUTH_SOCK}) \
-        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
         -e "pkg_dir_uid=${UID}" \
+        ${SSH_SOCK_ARGS} \
         davewongillies/fpm-recipes:${DISTRO} run.sh ${RECIPE_RB:-recipe.rb}
 else
     echo "You forgot the package dir you want to build"
