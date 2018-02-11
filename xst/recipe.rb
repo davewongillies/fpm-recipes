ba4f02b34f2c8e9da802f2870bf152e9cdca275b
Dave Wongillies <dave.gillies@gmail.com>
2018-02-10 19:46:36 -0800

    xst: don't package terminfo file

--- xst/recipe.rb
+++ xst/recipe.rb
@@ -1,7 +1,7 @@
 class Xst < FPM::Cookery::Recipe
   name     'xst'
   version  '0.7.1'
-  revision '3'
+  revision '4'
 
   description 'xst is a fork of st, which is a simple terminal implementation for X.'
   license     'MIT'
@@ -23,7 +23,6 @@ class Xst < FPM::Cookery::Recipe
   def install
     bin.install 'st'
     man1.install 'doc/st.1'
-    info.install 'doc/st.info'
     doc(name).install ['doc/FAQ', 'doc/LEGACY', 'doc/LICENSE', 'doc/TODO.md', 'doc/Xresources']
     share('applications').install workdir('st.desktop')
   end
