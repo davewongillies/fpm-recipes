befedf24120a61cfe36f85a97779f49af2e63287
David Gillies <dave.gillies@gmail.com>
2018-09-13 21:49:19 -0700

    trailer-cli: clean up recipe

--- trailer-cli/recipe.rb
+++ trailer-cli/recipe.rb
@@ -1,7 +1,7 @@
 class TrailerCli < FPM::Cookery::Recipe
   name     'trailer-cli'
   version  '1.0.8'
-  revision '1'
+  revision '2'
 
   maintainer 'Dave Wongillies <dave.gillies@gmail.com>'
   license    'GPL-2.0'
@@ -9,19 +9,22 @@ class TrailerCli < FPM::Cookery::Recipe
   build_depends 'swift-lang'
 
   homepage 'https://github.com/ptsochantaris/trailer-cli'
-  source   "#{homepage}.git", :with => :git, :tag => version
+  source   "#{homepage}.git",
+    :with => :git,
+    :tag  => version
 
   description 'Managing Pull Requests and Issues For GitHub & GitHub Enterprise from the command line
 '
 
   def build
-    safesystem 'swift build -c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization'
-    safesystem 'strip $(swift build -c release --show-bin-path)/trailer'
-    safesystem 'cp $(swift build -c release --show-bin-path)/trailer .'
+    build_options = '-c release -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization'
+    safesystem "swift build #{build_options}"
   end
 
   def install
-    bin.install 'trailer'
+    trailer_bin_path = `swift build -c release --show-bin-path`.strip + '/trailer'
+    safesystem  "strip #{trailer_bin_path}"
+    bin.install trailer_bin_path, 'trailer'
   end
 
 end
