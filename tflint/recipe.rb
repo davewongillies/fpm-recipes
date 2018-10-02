e93cfc4c103f25872a266297c7f7757270e2b65d
David Gillies <dave.gillies@gmail.com>
2018-10-02 00:03:00 -0700

    tflint: 0.7.2

--- tflint/recipe.rb
+++ tflint/recipe.rb
@@ -2,7 +2,7 @@ class Tflint < FPM::Cookery::Recipe
   description 'TFLint is a Terraform linter for detecting errors that can not be detected by terraform plan'
 
   name       'tflint'
-  version    '0.5.3'
+  version    '0.7.2'
   revision   '1'
   homepage   "https://github.com/wata727/#{name}"
   source     "#{homepage}/releases/download/v#{version}/#{name}_linux_amd64.zip"
