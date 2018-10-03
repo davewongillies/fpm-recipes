d538ae8f41807863530da40b85d5c94f5f7978f7
David Gillies <dave.gillies@gmail.com>
2018-10-02 21:10:17 -0700

    swift-lang: fix to build on bionic

--- swift-lang/recipe.rb
+++ swift-lang/recipe.rb
@@ -1,18 +1,37 @@
 class Swift < FPM::Cookery::Recipe
   name     'swift-lang'
   version  '4.2'
-  revision '1'
+  revision '2'
 
   maintainer 'Dave Wongillies <dave.gillies@gmail.com>'
   license    'Apache-2.0'
-  depends    'clang', 'clang-5.0', 'python', 'libicu55', 'libcurl3', 'libxml2'
-
-  osrel = FPM::Cookery::Facts.osrelease.sub('.', '')
 
   homepage "https://swift.org"
-  source   "#{homepage}/builds/swift-#{version}-release/#{FPM::Cookery::Facts.platform}#{osrel}/swift-#{version}-RELEASE/swift-#{version}-RELEASE-#{FPM::Cookery::Facts.platform}#{FPM::Cookery::Facts.osrelease}.tar.gz"
-
   description 'Swift is a general-purpose programming language built using a modern approach to safety, performance, and software design patterns.'
+
+  common_deps = 'clang', 'clang-5.0', 'python', 'libxml2'
+
+  case FPM::Cookery::Facts.platform
+  when :ubuntu
+    case FPM::Cookery::Facts.osrelease
+    when '18.04'
+      deps = 'libicu60', 'libcurl4'
+    when '16.04'
+      deps = 'libicu55', 'libcurl3'
+    end
+  end
+
+  depends deps + common_deps
+
+  osrelease  = FPM::Cookery::Facts.osrelease
+  osplatform = FPM::Cookery::Facts.platform
+  osrel      = osrelease.sub('.', '')
+
+  source "#{homepage}/builds/" + \
+    "swift-#{version}-release/" + \
+    "#{osplatform}#{osrel}/" + \
+    "swift-#{version}-RELEASE/" + \
+    "swift-#{version}-RELEASE-#{osplatform}#{osrelease}.tar.gz"
 
   def build; end
 
