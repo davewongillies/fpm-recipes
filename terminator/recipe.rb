850f6cb05ec3e5132ffff21bc795a37c68450788
David Gillies <dave.gillies@gmail.com>
2021-05-26 09:55:09 -0700

    terminator: 2.1.1

--- terminator/recipe.rb
+++ terminator/recipe.rb
@@ -1,29 +1,37 @@
 class Terminator < FPM::Cookery::Recipe
   name       'terminator'
-  version    '2.0.1'
+  version    '2.1.1'
   # git_ref =  '9e05a5d'
   # revision   "1~g#{git_ref}"
-  revision   "2"
+  revision   "1"
   homepage   'https://github.com/gnome-terminator/terminator'
   maintainer 'Dave Wongillies <dave.gillies@gmail.com>'
   license    'GPL-2.0'
   source     "https://github.com/gnome-terminator/#{name}.git",
     :with => 'git',
-    # :sha  => git_ref
     :tag  => "v#{version}"
+    # :sha  => git_ref
 
   description 'multiple GNOME terminals in one window'
 
-  build_depends 'python3-setuptools', 'python3-babel'
-  depends       'python3',
-                'python3-gi',
-                'python3-gi-cairo',
-                'python3-psutil',
-                'gir1.2-keybinder-3.0',
-                'gir1.2-vte-2.91',
-                'gettext',
+  build_depends 'gettext',
                 'intltool',
-                'dbus-x11'
+                'python3-babel',
+                'python3-setuptools'
+
+  depends 'dbus-x11',
+          'gir1.2-glib-2.0',
+          'gir1.2-gtk-3.0',
+          'gir1.2-keybinder-3.0',
+          'gir1.2-pango-1.0',
+          'gir1.2-vte-2.91',
+          'python3',
+          'python3-cairo',
+          'python3-configobj',
+          'python3-dbus',
+          'python3-gi',
+          'python3-gi-cairo',
+          'python3-psutil'
 
   def build
     safesystem 'python3 setup.py build'
